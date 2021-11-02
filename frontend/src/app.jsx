cfd98079e8f9571b9496848c9d33eab0e0015650
이재윤 <ag502@naver.com>
2021-11-02 22:48:22 +0900

    Add body color global css

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import NormalizeCss from './static/style/NormalizeCss';
+import GlobalCss from './static/style/GlobalCss';
 import { Header } from './components/common';
 import Main from './pages/Main';
 
 function App() {
     return (
         <>
+            <GlobalCss />
             <NormalizeCss />
             <Router>
                 <Header />

70a44c2d273fa2df9c11594b470fa9f93f00a98e
이재윤 <ag502@naver.com>
2021-11-02 22:54:57 +0900

    Add mypost page

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -5,6 +5,7 @@ import NormalizeCss from './static/style/NormalizeCss';
 import GlobalCss from './static/style/GlobalCss';
 import { Header } from './components/common';
 import Main from './pages/Main';
+import MyPost from './pages/MyPost';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Header />
                 <Switch>
                     <Route exact path='/' component={Main} />
+                    <Route exact path='/mylist' component={MyPost} />
                 </Switch>
             </Router>
         </>

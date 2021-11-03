f3673dbdcfa486ab4aa1c8386096381e69515e6e
이재윤 <ag502@naver.com>
2021-11-04 03:25:32 +0900

    Add mypost axios

--- frontend/src/pages/MyPost/index.jsx
+++ frontend/src/pages/MyPost/index.jsx
@@ -1,68 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import { PostContainer, PostCard } from '@/components/common';
 
-const posts = [
-    {
-        id: 1,
-        author: 'user1',
-        time: '2021-10-27',
-        title: 'This is Competition',
-        overView: 'This is summary',
-        thumbnail: '',
-    },
-    {
-        id: 2,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 3,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 4,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 5,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 6,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-    {
-        id: 7,
-        author: 'user1',
-        time: '2021-10-27',
-        title: '날 밀어 넣어, deep fake on me 준비가 안된 무대로, 몰아넣어, fake on me Got everybody mock up to me 수치를 느끼게 멘탈을 흔들어놔',
-        overView: `Get me, get me now, get me, get me now (zu, zu, zu, zu)지금 나를 잡아 아님 난 더 savage (zu, zu, zu, zu)Get me, get me now, get me, get me now (zu, zu, zu, zu) 이젠 내가 너를 잡아, now I'm a savage`,
-        thumbnail: 'https://via.placeholder.com/150',
-    },
-];
-
 function MyPost() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getMyEntirePosts = async () => {
+            try {
+                const result = await axios.get('http://localhost:8000/myposts');
+                setPosts(result.data);
+            } catch (error) {
+                alert('조회실패');
+                setPosts([]);
+            }
+        };
+        getMyEntirePosts();
+    }, []);
+
     return (
         <MyPostPageContainer>
             <PostContainer>

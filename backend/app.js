8322523bc5f157eb6cb7312754de1b58822bea5b
이재윤 <ag502@naver.com>
2021-11-04 01:06:34 +0900

    Add db model

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const { sequelize } = require('./models');
+
 const app = express();
 const port = 8000;
+
+sequelize.sync();
 
 app.listen(port, () => {
     console.log(`listening at ${port}`);

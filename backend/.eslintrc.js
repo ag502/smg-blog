c74e5eabadbe829471e32d31810b02cba39eda6c
이재윤 <ag502@naver.com>
2021-11-03 22:26:27 +0900

    Add eslint prettier

---
+++ backend/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+};
